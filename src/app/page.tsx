850e08098d7b80ef85a287aa475dd456ffd1636c
Danny Millea <danny@Dannys-Laptop-9.local>
2024-07-18 06:43:23 -0700

    added the typescript and basic movie files and API keys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
 import Banner from "./components/Banner";
 import Header from "./components/Header";
-export default function Home() {
+import requests from "./utils/requests";
+export default async function Home() {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 tp-[#010511] lg:h-[140vh]">
       <Header />
